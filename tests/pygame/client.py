5b3e375a6de336adbca724099061eb020aa0a219
iamdeedz <121265222+iamdeedz@users.noreply.github.com>
2023-10-26 19:06:40 +0100

    Change file layout for upload to pypi

--- tests/pygame/client.py
+++ tests/pygame/client.py
@@ -1,19 +1,34 @@
 from pymultiplayer import MultiplayerClient
 from json import dumps, loads
+from player import Player
 import pygame as p
 
 id = None
+self = None
+other_players = []
 
 
 async def main():
-    pass
+    for event in p.event.get():
+        if event.type == p.QUIT:
+            global running
+            running = False
 
 
 async def msg_handler(msg_json, client):
-    pass
+    if msg["type"] == "client_joined":
+        global other_players
+        other_players.append(Player(msg["content"]))
+
 
 async def proxy(websocket):
-    await client.msg_handler()
+    global self
+    self = Player(id)
+    while running:
+        await client.msg_handler()
+        await main()
+
+    await websocket.close()
 
 
 if __name__ == "__main__":
@@ -21,6 +36,7 @@ if __name__ == "__main__":
     p.display.set_caption("Multiplayer Test")
     screen = p.display.set_mode((500, 500))
     clock = p.time.Clock()
+    running = True
 
     client = MultiplayerClient(msg_handler)
     client.run(proxy)
