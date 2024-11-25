127460c36f839e31e5e7fecd25c104eb0ab8cd63
iamdeedz <121265222+iamdeedz@users.noreply.github.com>
2024-11-25 11:26:49 +0000

    Added clause for server creation

--- src/pymultiplayer/server_manager.py
+++ src/pymultiplayer/server_manager.py
@@ -6,10 +6,12 @@ import websockets, asyncio
 
 
 class ServerManager:
-    def __init__(self, ip, port, max_servers):
+    def __init__(self, ip, port, max_servers, init_func):
         self.ip = ip
         self.port = port
         self.max_servers = max_servers
+        self.init_func = init_func # Function ran to initialise a new server
+
         self.servers = list()
         self.servers.append(1301)
         self.servers.append(1303)
@@ -19,6 +21,10 @@ class ServerManager:
         if msg["type"] == "get":
             return_msg = dumps({"type": "get", "servers": self.servers})
             await websocket.send(return_msg)
+
+        elif msg["type"] == "create":
+            if len(self.servers)+1 <= self.max_servers:
+                self.init_func(msg["parameters"])
 
     async def _run(self):
         try:
