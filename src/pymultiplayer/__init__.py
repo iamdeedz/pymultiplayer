ad274e6938f7d6e1575a5d712f4ac30ec5fedd23
iamdeedz <121265222+iamdeedz@users.noreply.github.com>
2025-08-08 19:16:11 +0100

    added ServerOptions to __init__.py
    and also made the print statements far less annoying

--- src/pymultiplayer/__init__.py
+++ src/pymultiplayer/__init__.py
@@ -1,8 +1,8 @@
-from .TCPserver import TCPMultiplayerServer
+from .TCPserver import TCPMultiplayerServer, ServerOptions
 from .client import MultiplayerClient
 from .dynamic_server_manager import DynamicServerManager
 from .static_server_manager import StaticServerManager
 from .functions import get_servers, create_server, get_player_count_of_server
-print("pymultiplayer by iamdeedz")
-print("https://www.github.com/iamdeedz/pymultiplayer")
-print("This library is currently a work in progress.")
+
+if __name__ == "__main__":
+    print("github.com/iamdeedz/pymultiplayer - v0.3.6-dev5")
