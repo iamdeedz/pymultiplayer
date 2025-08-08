b5b052bfa2fd1be59092d00fc70051574ecf0346
iamdeedz <121265222+iamdeedz@users.noreply.github.com>
2025-08-08 19:55:49 +0100

    advance to dev6

--- src/pymultiplayer/__init__.py
+++ src/pymultiplayer/__init__.py
@@ -5,4 +5,4 @@ from .static_server_manager import StaticServerManager
 from .functions import get_servers, create_server, get_player_count_of_server
 
 if __name__ == "__main__":
-    print("github.com/iamdeedz/pymultiplayer - v0.3.6-dev5")
+    print("github.com/iamdeedz/pymultiplayer - v0.3.6-dev6")
