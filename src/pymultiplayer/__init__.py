e690e7a97556d901811747e29398a476dfee4903
iamdeedz <121265222+iamdeedz@users.noreply.github.com>
2025-08-08 20:11:27 +0100

    scrap the print statement all together
    Because it appears every time a new thread is started for some reason

--- src/pymultiplayer/__init__.py
+++ src/pymultiplayer/__init__.py
@@ -3,6 +3,3 @@ from .client import MultiplayerClient
 from .dynamic_server_manager import DynamicServerManager
 from .static_server_manager import StaticServerManager
 from .functions import get_servers, create_server, get_player_count_of_server
-
-if __name__ == "__main__":
-    print("github.com/iamdeedz/pymultiplayer - v0.3.6-dev6")
