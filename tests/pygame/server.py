1b7d312dd0e55adeb441b10863eca1971c6dae36
iamdeedz <121265222+iamdeedz@users.noreply.github.com>
2025-11-26 18:07:51 +0000

    change ip to 0.0.0.0

--- tests/pygame/server.py
+++ tests/pygame/server.py
@@ -39,5 +39,5 @@ async def client_left(server, client):
 
 
 if __name__ == "__main__":
-    server = TCPMultiplayerServer(ServerOptions(msg_handler, client_joined_func=client_joined, client_left_func=client_left))
+    server = TCPMultiplayerServer(ServerOptions(msg_handler, ip="0.0.0.0", client_joined_func=client_joined, client_left_func=client_left))
     server.run()
